3cab2ff221dd7520f8692e79e867ab6ed2d50c28
정원묵/원급(외부)/해체기술연구부 <9013211@kaeri.net>
2021-07-09 12:42:14 +0900

    o3d test

---
+++ o3dtest.py
@@ -0,0 +1,26 @@
+import open3d as o3d
+import numpy as np
+import pandas as pd
+import os
+from tqdm import tqdm
+
+ply_folder_path = r"C:\Users\pc\Desktop\Data\ply"
+ply_list = sorted(os.listdir(ply_folder_path))
+
+pcd_test = o3d.geometry.PointCloud()
+
+for i in tqdm(range(len(ply_list))):
+    pcd_load = o3d.io.read_point_cloud(os.path.join(ply_folder_path,ply_list[i]))
+    pcd_load = pcd_load.voxel_down_sample(voxel_size=0.001)
+    # pcd_print.append(pcd_load)
+    pcd_test += pcd_load
+# arr = np.array(pcd_load.points)
+
+print(type(pcd_test))
+
+
+# data_xyz = pd.DataFrame(arr, columns =["x", "y", "z"])
+
+# print(data_xyz)
+
+o3d.visualization.draw_geometries_with_editing([pcd_test])
