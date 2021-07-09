fff6177c461c837a2186996a8b71105152fd6172
정원묵/원급(외부)/해체기술연구부 <9013211@kaeri.net>
2021-07-09 13:33:56 +0900

    o3d test

--- o3dtest.py
+++ o3dtest.py
@@ -9,13 +9,15 @@ ply_list = sorted(os.listdir(ply_folder_path))
 
 pcd_test = o3d.geometry.PointCloud()
 
-for i in tqdm(range(len(ply_list))):
-    pcd_load = o3d.io.read_point_cloud(os.path.join(ply_folder_path,ply_list[i]))
-    pcd_load = pcd_load.voxel_down_sample(voxel_size=0.001)
-    # pcd_print.append(pcd_load)
-    pcd_test += pcd_load
+# for i in tqdm(range(len(ply_list))):
+#     pcd_load = o3d.io.read_point_cloud(os.path.join(ply_folder_path,ply_list[i]))
+#     pcd_load = pcd_load.voxel_down_sample(voxel_size=0.001)
+#     # pcd_print.append(pcd_load)
+#     pcd_test += pcd_load
 # arr = np.array(pcd_load.points)
 
+pcd_load = o3d.io.read_point_cloud(os.path.join(ply_folder_path,ply_list[0]))
+pcd_load = pcd_load.voxel_down_sample(voxel_size=0.0001)
 print(type(pcd_test))
 
 
@@ -23,4 +25,4 @@ print(type(pcd_test))
 
 # print(data_xyz)
 
-o3d.visualization.draw_geometries_with_editing([pcd_test])
+o3d.visualization.draw_geometries_with_editing([pcd_load])
