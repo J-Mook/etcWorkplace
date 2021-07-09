7000d182e86b7a39d05fb0f2f921ff0204622508
정원묵 <jm@MacBook-Air-2.local>
2021-07-09 13:33:00 +0900

    dd

--- intersection.py
+++ intersection.py
@@ -12,14 +12,16 @@ def creat_mesh():
 	# ply_name = 'temp.png'
 
 	ply_list = sorted(os.listdir(ply_folder_path + '/data'))
-
+	
 	pcd_test = o3d.geometry.PointCloud()
-
-	for ply in ply_list:
-		pcd_load = o3d.io.read_point_cloud(os.path.join(ply_folder_path,ply))
+	# for ply in ply_list:
+	# 	pcd_load = o3d.io.read_point_cloud(os.path.join(ply_folder_path,ply))
 
 	pcd_load = o3d.io.read_point_cloud(os.path.join(ply_folder_path,ply_list[0]))
-	o3d.visualization.draw_geometries_with_editing([pcd_load])
+	pcd_load = pcd_load.voxel_down_sample(voxel_size=0.001)
+	print(type(pcd_load))
+	print(pcd_load)
+	o3d.visualization.draw_geometries([pcd_load])
 
 def LinePlaneCollision(planeNormal, planePoint, rayDirection, rayPoint, epsilon=1e-6):
  
@@ -35,14 +37,14 @@ def LinePlaneCollision(planeNormal, planePoint, rayDirection, rayPoint, epsilon=
 
 if __name__=="__main__":
 	#Define plane
-	planeNormal = np.array([0, 0, 2])
-	planePoint = np.array([0, 0, 5]) #Any point on the plane
+	# planeNormal = np.array([0, 0, 2])
+	# planePoint = np.array([0, 0, 5]) #Any point on the plane
  
-	#Define ray
-	rayDirection = np.array([0, -1, 1])
-	rayPoint = np.array([0, 0, 10]) #Any point along the ray
+	# #Define ray
+	# rayDirection = np.array([0, -1, 1])
+	# rayPoint = np.array([0, 0, 10]) #Any point along the ray
 	
-	Psi = LinePlaneCollision(planeNormal, planePoint, rayDirection, rayPoint)
-	print ("intersection at", Psi)
+	# Psi = LinePlaneCollision(planeNormal, planePoint, rayDirection, rayPoint)
+	# print ("intersection at", Psi)
 
-	
+	creat_mesh()
