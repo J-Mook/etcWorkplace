bf3ba386fca9fa9bc419ecebfe52dc24f876f272
정원묵 <jm@MacBook-Air-2.local>
2021-07-11 17:53:52 +0900

    raycast

--- codetest.py
+++ codetest.py
@@ -1,14 +1,23 @@
-def solution(n, costs):
+from pycaster.pycaster import rayCaster
+# import rayCaster
+import os
+import inspect
 
-    costs = sorted(costs, key = lambda x : (x[2], x[1]))
-    last = costs[0][0]
-    answer = 0
-    for cost in costs:
+print(inspect.getfile(rayCaster))
+
+
+
+# def solution(n, costs):
+
+#     costs = sorted(costs, key = lambda x : (x[2], x[1]))
+#     last = costs[0][0]
+#     answer = 0
+#     for cost in costs:
         
-        if last + 1 == cost[0]:
-            answer += cost[2] 
-            last = cost[1]
+#         if last + 1 == cost[0]:
+#             answer += cost[2] 
+#             last = cost[1]
 
-    return answer
+#     return answer
 
-print(solution(4, [[0,1,1],[0,2,2],[1,2,5],[1,3,1],[2,3,8]]))
+# print(solution(4, [[0,1,1],[0,2,2],[1,2,5],[1,3,1],[2,3,8]]))
--- raycast.py
+++ raycast.py
@@ -1,20 +1,81 @@
-import pycaster # it should run on python2 environmonet
+from pycaster.pycaster import rayCaster
 import os
 import open3d as o3d
-from pycaster.test import test_all
+import numpy as np
+from tqdm import tqdm
+import math
+# from pycaster.test import test_all
 
-ply_folder_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
+def rotation_matrix(axis, theta):
+    
+    axis = np.asarray(axis)
+    axis = axis / math.sqrt(np.dot(axis, axis))
+    a = math.cos(math.degrees(theta))
+    b, c, d = -axis * math.sin(math.degrees(theta))
+    aa, bb, cc, dd = a * a, b * b, c * c, d * d
+    bc, ad, ac, ab, bd, cd = b * c, a * d, a * c, a * b, b * d, c * d
+    return np.array([[aa + bb - cc - dd, 2 * (bc + ad), 2 * (bd - ac)],
+                     [2 * (bc - ad), aa + cc - bb - dd, 2 * (cd + ab)],
+                     [2 * (bd + ac), 2 * (cd - ab), aa + dd - bb - cc]])
+
+def make_pcd_srf2srf():
+    # surface to surface
+    for i in tqdm(range(-500,500,point_dencity)):
+        for j in range(-500,500,point_dencity):
+            pSource = [i, -1000.0, j]
+            pTarget = [i, 1000.0, j]
+            pSource = np.dot(rotation_matrix(axis, theta), pSource)
+            pTarget = np.dot(rotation_matrix(axis, theta), pTarget)
+            
+            try:
+                pointsIntersection = caster.castRay(pSource, pTarget)
+                pcd_list.append(pointsIntersection[0])
+                # pcd_list.append(pointsIntersection[1])
+                
+            except:
+                pass
+
+def make_pcd_srf2pnt():
+    # point to surface
+    pSource = [500, -1000.0, 500]
+    for i in tqdm(range(-2000,2000,point_dencity)):
+        for j in range(-2000,2000,point_dencity):
+            pTarget = [i, 500.0, j]
+            # pSource = np.dot(rotation_matrix(axis, theta), pSource)
+            # pTarget = np.dot(rotation_matrix(axis, theta), pTarget)
+            
+            try:
+                pointsIntersection = caster.castRay(pSource, pTarget)
+                pcd_list.append(pointsIntersection[0])
+                # pcd_list.append(pointsIntersection[1])
+                
+            except:
+                pass
+
+
+stl_folder_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data")
 	
 # ply_list = sorted(os.listdir(ply_folder_path))
 file_name = "TestSpecimenAssy.stl"
-file = os.path.join(ply_folder_path, file_name)
+file = os.path.join(stl_folder_path, file_name)
 
-ply_mesh = o3d.io.read_triangle_mesh(os.path.join(file))
+caster = rayCaster.fromSTL(file, scale = 1)
+pcd_list = []
 
-caster = pycaster.rayCaster.fromSTL(file)
+point_dencity = 20
+axis = [1, 1, 1]
+theta = -30
 
-pSource = [100.0, 100.0, 0.0]
-pTarget = [0.0, 0.0, 0.0]
-pointsIntersection = caster.castRay(pSource, pTarget)
+make_pcd_srf2pnt()
 
-test_all.runTests()
+pcd_array = np.asarray(pcd_list, dtype=np.float32)
+
+print(pcd_array)
+
+pcd = o3d.geometry.PointCloud()
+pcd.points = o3d.utility.Vector3dVector(pcd_array)
+
+# pcd = o3d.io.read_point_cloud(file)
+# pcd = pcd.voxel_down_sample(voxel_size=0.001)
+
+o3d.visualization.draw_geometries_with_editing([pcd])
